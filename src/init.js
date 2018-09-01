d75b80e46b9315073d4b9224d62ad8cc1c3f44c0
macaguegi <macaguegi@yahoo.es>
2018-09-01 13:14:18 -0500

    ultima version de parte visual en phaser

--- src/init.js
+++ src/init.js
@@ -1,15 +1,17 @@
 var teclado;
 var puntaje=500;
+var flagSebastian = true;
+
 const config = {
-    width: 320*4,
-    height: 180*4,
+    width: 320*8,
+    height: 180*8,
     parent: "container",
     type: Phaser.AUTO,
     physics: {
-        default: 'arcade'
-       /*  arcade: {
-            gravity: { y: 5 }
-        } */
+        default: 'arcade',
+         arcade: {
+            gravity: { y: 20 }
+        } 
     },
     scene: {
         preload: preload,
@@ -25,7 +27,7 @@ function preload(){
 //Precarga de imagenes,sprites, sonidos, teclado.
     teclado = this.input.keyboard.createCursorKeys();
     console.log("Soy preload");
-    this.load.image("logo", "assets/logo.png");
+    this.load.image("logo", "assets/doc/logo.png");
     this.load.image("fondoT", "assets/bg/wallTemplado.png");
     this.load.image("fondoF", "assets/bg/cold.jpg");
     this.load.image("fondoN", "assets/bg/snow.jpg");
@@ -43,7 +45,7 @@ function create(){
     console.log("Soy create");
 
 //Adicion imagenes y sprites
-    this.add.image(500,120, "fondoN");
+    this.add.image(500,300, "fondoN").setScale(0.9,0.9);
     var can = this.add.image(1200,570, "caneca").setScale(0.2,0.2);
 
     
@@ -73,12 +75,22 @@ var cindyThrow = {
     repeat: -1
 };
 
+var cindyJumps = {
+    key: "cindySalta",
+    frames : this.anims.generateFrameNumbers('cindy',{start:7, end:13}),
+    frameRate : 5,
+    yoyo : false,
+    repeat: -1
+};
+
 
     cindyC = this.anims.create(cindyWalk);
     cindyA = this.anims.create(cindyThrow);
+    cindyS = this.anims.create(cindyJumps);
 
     console.log(cindyC);
     console.log(cindyA);
+    console.log(cindyS);
 
     cin = this.add.sprite(70, 600, "cindy");
     cin.setDisplaySize(180,180)
@@ -87,48 +99,81 @@ var cindyThrow = {
 
     cin.anims.load("cindyCamina");
     cin.anims.load("cindyArroja");
+    cin.anims.load("cindySalta");
+    
 
     this.physics.world.enable([ cin ]);
     cin.body.setVelocity(0,0);
+    cin.body.setAllowGravity(false);
 
     this.physics.world.enable([ can ]);
     can.body.setVelocity(0,0);
+    can.body.setAllowGravity(false);
 
     this.add.image(1150,150, "logo").setScale(0.3);
-    var ba1 = this.add.image(600,600, "basura1").setScale(2,2);
-    var ba2 = this.add.image(800,600, "basura2").setScale(2,2);
-    this.add.image(360,360, "basura3").setScale(2,2);
-    this.add.image(480,480, "basura4").setScale(2,2);
 
+    var ba1 = this.add.image(600,600, "basura1").setScale(2,2);
+    
+
+    var ba2 = this.add.image(800,600, "basura2").setScale(2,2);
+
+    var ba3 = this.add.image(360,360, "basura3").setScale(2,2);
+    var ba4 = this.add.image(480,480, "basura4").setScale(2,2);
+    this.physics.world.enable([ ba1, ba2, ba3, ba4 ]);
+
+    ba1.body.setAllowGravity(false);
+    ba2.body.setAllowGravity(false);
+    ba3.body.setAllowGravity(false);
+    ba4.body.setAllowGravity(false);
+
+    ba1.body.setCollideWorldBounds(true);
+    ba2.body.setCollideWorldBounds(true);
+    ba3.body.setCollideWorldBounds(true);
+    ba4.body.setCollideWorldBounds(true);
 
    
 
 //Events
-    /* this.input.keyboard.on('keydown_SPACE', function (event) {
-        if(x){
-            cin.anims.play("cindyArroja");
+    this.input.keyboard.on('keydown_SPACE', function (event) {
+        alert("Se presiono space");
+        cin.anims.play('cindyArroja');
+        if(ba1.body.x-cin.body.x >= -50 && ba1.body.x-cin.body.x <=50){
+            puntaje += 200;
+            this.physics.world.collide(cin, ba1);
+            console.log(puntaje);
         }else{
-            cin.anims.play("cindyArroja");
+            puntaje -= 100;
+            console.log(puntaje);
         }
         
 
     });
- */
+ 
 
-    this.input.keyboard.on('keydown_UP', function (event) {
+     this.input.keyboard.on('keydown_UP',  (event) => {    
+        console.log(cin.body.y);
+        
+        while(cin.body.y <= 300){
+            console.log("entré");
+            cin.body.velocity.y -= 20;
+            cin.body.y += 100;
+            cin.anims.play("cindySalta");
+            cin.body.setAllowGravity(true); 
+        }      
 
-        //console.log("Aqui deberias saltar");
-
-    });
+    }); 
 
 
    
     this.input.keyboard.on("keydown_RIGHT", function (event) {
             console.log("hola mundo");
+
            if (cin.flipX==true){
             cin.flipX=false;
             cin.anims.play("cindyCamina");
-            cin.body.velocity.x += 40;}
+            cin.body.velocity.x += 40;
+            cin.body.velocity.y -= 40;
+        }
             else{    
                 cin.anims.play("cindyCamina");
                 cin.body.velocity.x += 40;
@@ -154,7 +199,30 @@ function update(time, delta){
     if(teclado.down.isDown) {
         console.log("Abajo");
     } 
-    if(teclado.up.isDown) {
+
+   
+        if(teclado.up.isDown){
+            cin.body.y -= 10;
+            cin.anims.play("cindySalta");
+        }
+
+/*     
+    if(teclado.up.isDown && cin.body.y==510 && cin.body.y>0) {
         console.log("Arriba");
-    } 
+        cin.body.velocity.y -= 20;
+        cin.body.y += 100;
+        cin.anims.play("cindySalta");
+        cin.body.setAllowGravity(true);
+    }else if (!teclado.up.isDown && cin.body.y > 510 ){
+        cin.body.velocity.y += 6; 
+        cin.body.setAllowGravity(false);
+    } */
+       
+       /*  cin.body.velocity.y -= 20;
+        cin.body.y += 100;
+        cin.anims.play("cindySalta");
+        cin.body.setAllowGravity(true); */
+
+    
+   
 }
